3ffebc03f01bce59e93fb8d3833275e1df7d67b4
Nataliia Tsirul <tcirulnatalya@gmail.com>
2024-07-06 11:31:22 +0100

    added AppBanner section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 // import React from 'react'
+import AppBanner from './components/AppBanner/AppBanner';
 import Hero from './components/Hero/Hero';
 import Services from './components/Services/Services';
 import WhereToBuy from './components/WhereToBuy/WhereToBuy';
+
+
 const App = () => {
   return (
     <div className="overflow-x-hidden">
       <Hero />
       <Services />
       <WhereToBuy />
+      <AppBanner />
+     
     </div>
   );
 };
