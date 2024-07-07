a17b8aa1da94397b92a915688062956f62c9a370
Nataliia Tsirul <tcirulnatalya@gmail.com>
2024-07-07 11:46:33 +0100

    adde motion to Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -17,7 +17,16 @@ const Footer = () => {
       <div className="container">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
           {/* Company details sevtion */}
-          <div className="space-y-6">
+          <motion.div
+            initial={{ opacity: 0, y: 100 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{
+              delay: 0.2,
+              duration: 0.6,
+            }}
+            className="space-y-6"
+          >
             <h1 className="text-3xl font-bold uppercase">Coders Cafe</h1>
             <p className="text-sm max-w-[300px]">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
@@ -33,14 +42,23 @@ const Footer = () => {
                 <FaMapLocation /> Staffors, 10 Rassel Street, SM18 5RE
               </p>
             </div>
-          </div>
+          </motion.div>
           {/* Footer Links sevtion */}
-          <div className="space-y-6">
+          <motion.div
+            initial={{ opacity: 0, y: 100 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{
+              delay: 0.4,
+              duration: 0.6,
+            }}
+            className="space-y-6"
+          >
             <h2 className="text-3xl font-bold">Quick Links</h2>
             <div className="grid grid-cols-2  gap-3">
               {/* first column section */}
               <div>
-                <ul className='space-y-2'>
+                <ul className="space-y-2">
                   <li>Home</li>
                   <li>About</li>
                   <li>Contact Us</li>
@@ -49,7 +67,7 @@ const Footer = () => {
               </div>
               {/* second column section */}
               <div>
-                <ul className='space-y-2'>
+                <ul className="space-y-2">
                   <li>Home</li>
                   <li>About</li>
                   <li>Contact Us</li>
@@ -57,24 +75,36 @@ const Footer = () => {
                 </ul>
               </div>
             </div>
-          </div>
+          </motion.div>
           {/* Social Links  sevtion */}
-          <div className="space-y-6">
-            <h2 className='text-3xl font-bold'>Follow Us</h2>
-            <div className='flex gap-3 items-center'>
-              <FaFacebook className='text-3xl hover:scale-105 duration-300'/>
-              <FaInstagram className='text-3xl hover:scale-105 duration-300' />
-              <FaTelegram className='text-3xl hover:scale-105 duration-300'/>
-              <FaGoogle className='text-3xl hover:scale-105 duration-300'/>
+          <motion.div
+            initial={{ opacity: 0, y: 100 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{
+              delay: 0.6,
+              duration: 0.6,
+            }}
+            className="space-y-6"
+          >
+            <h2 className="text-3xl font-bold">Follow Us</h2>
+            <div className="flex gap-3 items-center">
+              <FaFacebook className="text-3xl hover:scale-105 duration-300" />
+              <FaInstagram className="text-3xl hover:scale-105 duration-300" />
+              <FaTelegram className="text-3xl hover:scale-105 duration-300" />
+              <FaGoogle className="text-3xl hover:scale-105 duration-300" />
             </div>
-            <div className='space-y-5'>
-              <h2 className='text-xl font-bold'>Payment Methods</h2>
-        
-                <img src={CreditCards} alt='credit-cards' className='w-[80%]'/>
-            
+            <div className="space-y-5">
+              <h2 className="text-xl font-bold">Payment Methods</h2>
+
+              <img src={CreditCards} alt="credit-cards" className="w-[80%]" />
             </div>
-          </div>
+          </motion.div>
+          {/* copyright section */}
         </div>
+        <p className="text-white text-center mt-8 pt-8 border-t-2">
+          Copyright &copy; 2024 Company Name. All rights reserved.
+        </p>
       </div>
     </div>
   );
